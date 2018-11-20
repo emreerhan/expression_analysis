caffb65c167d1ffc51f7cd1ac2542561033f8aa1
Emre Erhan <eerhan@bcgsc.ca>
2018-11-20 13:34:53 -0800

    Add cardinality calculations

--- scripts/intersections.py
+++ scripts/intersections.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np 
 import itertools
+import argparse
 
 def main():
     test_sets = [{1,2,3,4}, {1,2,3}, {3,4,5,6}, {4,5}, {2,3,4}, {2,1,5}, {1,2,3,4}]
@@ -14,15 +15,14 @@ def get_intersections(sets):
     sets = np.array(sets)
     cardinalities = np.zeros(len(sets))
 
-    for i in range(2, len(sets)):
+    for i in range(1, len(sets)):
         for set_index_combo in itertools.combinations(range(len(sets)), i):
-            print(set_index_combo)
-            combo_size = len(set.intersection(*sets[list(set_index_combo)]))
-            row = np.zeros(len(sets), dtype=np.bool)
-            print(set_index_combo)
+            combo_cardinality = len(set.intersection(*sets[list(set_index_combo)]))
+            row = np.zeros(len(sets)+1)
+            row[len(row)-1] = combo_cardinality
             row[list(set_index_combo)] = True
             intersections_df = intersections_df.append(pd.Series(row), ignore_index=True)
-            print(pd.Series(row))
+
     return intersections_df
 
 if __name__ == '__main__':
